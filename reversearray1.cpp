a619b754fc06c9ad5b19420d8190b1d8438ad4ec
Ombangera1309 <105063662+Ombangera1309@users.noreply.github.com>
2022-05-25 17:15:00 +0530

    Add files via upload

---
+++ reversearray1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[5], i;
+   
+    cout<<endl<<"Enter the elements of the array :";
+     for(i=0; i<5; i++)
+    cin>>arr[i];
+    
+    cout<<"\n\nThe original array is :";
+    for(i=0; i<5; i++)
+    cout<<arr[i]<<" ";
+
+    cout<<"\n\nThe reversed array is :";
+    for(i=(5-1); i>=0; i--)
+    cout<<arr[i]<<" ";
+    cout<<endl;
+
+    return 0;
+
+}
